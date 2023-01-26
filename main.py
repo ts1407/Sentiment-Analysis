b3ca26ad87a3ce6626b7eaf495193a045545c577
ts1407 <98187632+ts1407@users.noreply.github.com>
2023-01-26 12:57:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import string
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+text = open('read.txt', encoding='utf-8').read()
+lower_case = text.lower()
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+tokenized_words = word_tokenize(cleaned_text, "english")
+
+final_words = []
+for word in tokenized_words:
+    if word not in stopwords.words('english'):
+        final_words.append(word)
+
+emotion_list = []
+with open('emotion.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace('\n', '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_words:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+
+def sentiment_analyze(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    neg = score['neg']
+    pos = score['pos']
+    if neg > pos:
+        print("Negative Sentment")
+    elif pos > neg:
+        print("Positive sentiment")
+    else:
+        print("Neutral Sentiment")
+
+
+sentiment_analyze((cleaned_text))
+
+fig,ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
